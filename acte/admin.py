b44d8de3806fe51d545bd8c5dd7e0138d124bc76
kathypuiu <kathypuiu@yahoo.com>
2017-08-24 15:11:23 +0300

    added database

--- acte/admin.py
+++ acte/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Document, Institution
 
+admin.site.register(Document)
+admin.site.register(Institution)
 # Register your models here.
--- acte/models.py
+++ acte/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
+# Baza de date a site-ului
+class Document(models.Model):
+    url_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, default='')
+    institution = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    requirements = JSONField()
+
+    def __str__(self):
+        return self.url_name
+
+class Institution(models.Model):
+    url_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    documents = JSONField()
+
+    def __str__(self):
+        return self.url_name
