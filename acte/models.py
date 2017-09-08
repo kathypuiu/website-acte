199c6754ceae96d19aee59cc7e5bded99b85e6e6
kathypuiu <kathypuiu@yahoo.com>
2017-09-08 17:27:56 +0300

    urls almost final

--- acte/models.py
+++ acte/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Baza de date a site-ului
 class Document(models.Model):
-    url_name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50, default='')
     institution = models.CharField(max_length=100)
@@ -15,7 +15,7 @@ class Document(models.Model):
         return self.url_name
 
 class Institution(models.Model):
-    url_name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     program = models.CharField(max_length=100)
--- acte/urls.py
+++ acte/urls.py
@@ -6,10 +6,17 @@ from . import views
 urlpatterns = [
    url(r'^$' , views.index , name ='index'),
    url(r'^documente$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/lista_acte.html")),
-   url(r'^documente/(?P<pk>\d+)$', DetailView.as_view(model = Document, template_name="acte/act.html")),
+   url(r'^documente/(?P<slug>\d+)$', DetailView.as_view(model = Document, template_name="acte/act.html")),
    url(r'^institutii$', ListView.as_view(queryset=Institution.objects.all().order_by("id"),template_name="acte/lista_institutii.html")),
-   url(r'^institutii/(?P<pk>\d+)$', DetailView.as_view(model = Institution, template_name="acte/institutie.html")),
-   url(r'^categorie/(?P<pk>\d+)$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^institutii/(?P<slug>[\w\-]+)/$', DetailView.as_view(model = Institution, template_name="acte/institutie.html")),
+   url(r'^categorie/auto$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/educational$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/etc$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/firma$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/identitate$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/imobiliar$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/social$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^categorie/medical$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
    url(r'^contact$', views.contact, name = 'contact'),
 
 
