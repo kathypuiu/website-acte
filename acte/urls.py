779d0c5c1a39498524110645a8a324b88f1272d3
kathypuiu <kathypuiu@yahoo.com>
2017-09-09 17:13:55 +0300

    url categorii

--- acte/urls.py
+++ acte/urls.py
@@ -6,17 +6,18 @@ from . import views
 urlpatterns = [
    url(r'^$' , views.index , name ='index'),
    url(r'^documente$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/lista_acte.html")),
-   url(r'^documente/(?P<slug>\d+)$', DetailView.as_view(model = Document, template_name="acte/act.html")),
+   url(r'^documente/(?P<slug>[\w\-]+)$', DetailView.as_view(model = Document, template_name="acte/act.html")),
    url(r'^institutii$', ListView.as_view(queryset=Institution.objects.all().order_by("id"),template_name="acte/lista_institutii.html")),
-   url(r'^institutii/(?P<slug>[\w\-]+)/$', DetailView.as_view(model = Institution, template_name="acte/institutie.html")),
-   url(r'^categorie/auto$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/educational$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/etc$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/firma$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/identitate$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/imobiliar$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/social$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
-   url(r'^categorie/medical$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/categorie.html")),
+   url(r'^institutii/(?P<slug>[\w\-]+)$', DetailView.as_view(model = Institution, template_name="acte/institutie.html")),
+
+   url(r'^categorie/auto$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/auto.html")),
+   url(r'^categorie/educational$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/educational.html")),
+   url(r'^categorie/etc$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/etc.html")),
+   url(r'^categorie/firma$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/firma.html")),
+   url(r'^categorie/identitate$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/identitate.html")),
+   url(r'^categorie/imobiliar$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/imobiliar.html")),
+   url(r'^categorie/social$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/social.html")),
+   url(r'^categorie/medical$', ListView.as_view(queryset=Document.objects.all().order_by("id"),template_name="acte/medical.html")),
    url(r'^contact$', views.contact, name = 'contact'),
 
 
