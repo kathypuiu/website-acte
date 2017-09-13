995f8812e32d6694c5d8239f948c7494451c0748
kathypuiu <kathypuiu@yahoo.com>
2017-09-13 17:08:00 +0300

    watson added

---
+++ acte/__init__.py
@@ -0,0 +1,1 @@
+#default_app_config = 'acte.apps.ActeConfig'
--- acte/apps.py
+++ acte/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from watson import search as watson
+from .models import Document, Institution
 
 class ActeConfig(AppConfig):
     name = 'acte'
+    def ready(self):
+        YourModel = self.get_model("Document")
+        watson.register(Document)
