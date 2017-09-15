b8134e47fc15951054f02f487d97d74b189ebf7b
kathypuiu <kathypuiu@yahoo.com>
2017-09-15 16:25:58 +0300

    search updated

--- acte/apps.py
+++ acte/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from watson import search as watson
 from .models import Document, Institution
 
 class ActeConfig(AppConfig):
     name = 'acte'
-    def ready(self):
-        YourModel = self.get_model("Document")
-        watson.register(Document)
