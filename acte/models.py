e3816b11389f7d45991dcfab91080a0d3f4aa241
kathypuiu <kathypuiu@yahoo.com>
2017-09-18 14:17:02 +0300

    finalization touches

--- acte/models.py
+++ acte/models.py
@@ -5,11 +5,12 @@ class Document(models.Model):
     slug = models.SlugField(max_length=50)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50, default='')
+    institution_slug = models.CharField(max_length=50, default='')
     institution = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     requirements = models.CharField(max_length=500)
-    links = models.CharField(max_length=100)
-    extra = models.CharField(max_length=500)
+    links = models.CharField(max_length=100, default='', null=True, blank=True)
+    extra = models.CharField(max_length=500, default='', null=True, blank=True)
 
     def __str__(self):
         return self.slug
@@ -20,7 +21,7 @@ class Institution(models.Model):
     address = models.CharField(max_length=100)
     program = models.CharField(max_length=100)
     documents = models.CharField(max_length=500)
-    links = models.CharField(max_length=100)
+    links = models.CharField(max_length=100, default='', null=True, blank=True)
 
     def __str__(self):
         return self.slug
